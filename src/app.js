b5beee261664b58547994c08642009c66d66b525
sujal sharma <sujalsukoimk5@gmail.com>
2025-06-25 16:23:24 +0530

    changed url

--- src/app.js
+++ src/app.js
@@ -5,11 +5,8 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: (origin, callback) => {
-        callback(null, origin); // Reflect the request origin
-    },
-}));app.use(express.json());
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
